5c3868578e8b7a624e1fe0f614fb36e6186a6f7c
quannm97 <quan.nm.dever@gmail.com>
2023-05-19 01:41:03 +0700

    done basic hover in

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 (function setupToolTip() {
+    /**
+     * track down DOM node
+     */
     let tooltip = "";
-    let tooltipDiv = document.querySelector(".div-tooltip")
-    let tooltipElements = Array.from(document.querySelectorAll(".hover-reveal"));
+    const tooltipDiv = document.querySelector(".div-tooltip");
+    const tooltipElements = Array.from(
+        document.querySelectorAll(".hover-reveal")
+    );
 
-    const displayTooltip = function (e, this) {
-        
-    }
+    /**
+     *
+     * @param {Event} e
+     * @param {object} elem
+     */
+
+    /**
+     * Display the tooltip
+     */
+
+    const displayTooltip = function (e, elem) {
+        // tooltip = elem.getAttribute("data-tooltip");
+        tooltip = elem.dataset.tooltip;
+        tooltipDiv.innerHTML = tooltip;
+        tooltipDiv.style.top = e.pageY + "px";
+        tooltipDiv.style.left = e.pageX + "px";
+        tooltipDiv.style.opacity = 1;
+    };
+
+    /**
+     * for loop all element in array to apply action
+     */
 
     tooltipElements.forEach(function (el) {
-        el.addEventListener("click", function (e, this) {
+        el.addEventListener("mouseenter", function (e) {
             displayTooltip(e, this);
-        })
-    })
+        });
+    });
 })();
